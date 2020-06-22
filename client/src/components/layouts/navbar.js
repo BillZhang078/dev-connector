c05fa4b853bd048ce62216ccad4bed5601a46bd1
Bill78Zhang <wohencool21@gmail.com>
2020-06-22 15:30:15 +0930

    create react app

---
+++ client/src/components/layouts/navbar.js
@@ -0,0 +1,11 @@
+import React from 'react-js'
+
+const Navbar = (props) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Navbar;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Profile = require('../../models/Profile')
 const User = require("../../models/User")
-const { check, validationResult } = require('express-validator/check')
+const { check, validationResult } = require('express-validator')
 // @route    GET api/profile
 // @desc     Test Route
 // @acess    private
@@ -19,7 +19,7 @@ router.get('/me', async (req, res) => {
     }
 });
 
-// @route    GET api/profile
+// @route    POST api/profile
 // @desc     Save and update Route
 // @acess    private
 
@@ -34,9 +34,115 @@ router.post('/', [check('status', 'status cannot be empty').not().isEmpty(),
         // Build Profile Object
         const profileField = {};
 
-        
+        profileField.user = req.user;
+        if (company) profileField.company = company;
+        if (location) profileField.location = location;
+        if (status) profileField.status = status;
+        if (skills) profileField.skills = skills.split(',').map(skill=>skill.trim())
+        if (website) profileField.company = company;
+        if (bio) profileField.bio = bio;
+        if (githubusername) profileField.githubusername = githubusername;
+        if (experience) profileField.experience = experience;
 
+        try {
+
+            const profile = await Profile.findOne({ user: req.user.id })
+            if (profile) {
+                profile = await Profile.findByIdAndUpdate({ user: req.user.id }, { $set: profileField }, { new: true });
+                return res.json(profile)
+            }
+            profile = new Profile();
+            await Profile.save();
+            return res.json(profile)
+            
+        } catch (err) {
+            console.error(err.Array())
+        }
+        
+        return res.json(profileField);
 
 
 })
+
+// @route    GET api/profiles
+// @desc     Get All Profiles
+// @acess    private
+
+router.get('/profiles', async (req, res)=>{
+    
+    try {
+
+        const profiles = await Profile.find().populate('user', ['name', 'avavtar'])
+        return res.json(profiles);
+        
+    } catch (error) {
+
+        res.status(500).send({ msg: 'server error' })
+        console.log('server error')
+    }
+}
+)
+
+
+// @route    GET api/profile/user/:user_id
+// @desc     Get Profile by userId
+// @acess    private
+
+router.get('/user/:user_id', async (req, res)=>{
+    
+    try {
+
+        const profile = await Profile.findOne({ user: req.user.id }).populate('user', ['name', 'avavtar'])
+        if (!profile) {
+            return res.status(400).json({msg:'There is no profile find for this user'})
+        }
+
+        return res.json(profile);
+        
+    } catch (error) {
+
+        console.log(error.message)
+        if (error.kind === 'objectId') {
+            return res.status(400).json({msg:" No Profile found"})
+        }
+        res.status(500).send({ msg: 'server error' })
+    } 
+}
+)
+
+// @route    Delete api/profiles
+// @desc     delete profiles and user
+// @acess    private
+
+router.delete('/', async (req, res)=>{
+    
+    try {
+
+        await Profile.findOneAndRemove({ user: req.user.id });
+        await User.findByIdAndRemove({ _id: req.user.id });
+        
+        res.json({ msg: 'User Deleted' });
+        
+    } catch (error) {
+
+        res.status(500).send({ msg: 'server error' })
+        console.log(error.message)
+    }
+}
+)
+
+// @route    Delete api/profiles
+// @desc     delete profiles and user
+// @acess    private
+
+// router.post('/',[],async (req,res)=>{
+//     try {
+
+                
+//     }catch (error) {
+//         res.status(500).send({ msg: 'server error' });
+//         console.log(error.message);
+//     }
+// })
+
 module.exports = router;
